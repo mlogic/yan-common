2da3c29ac488956bf270e17b2eb815c4ee13f2d7
Yan Li <yanli@tuneup.ai>
2020-09-19 21:31:51 -0700

    run-until-success.sh: various bug fixes

--- os/run-until-success.sh
+++ os/run-until-success.sh
@@ -4,11 +4,11 @@
 #
 # TODO:
 # 1. test cases
+set -euo pipefail
 
 . "${YAN_COMMON}/shell/_log.sh"
 . "${YAN_COMMON}/shell/_mutex.sh"
 
-set -euo pipefail
 readonly orig_cmd_opt=("$@")
 force=0
 stop_rc=0
@@ -35,7 +35,7 @@ while :; do
             break
             ;;
         *)
-            err "Unknown option: $1"
+            log error "Unknown option: $1"
             exit 2
             ;;
     esac
@@ -43,28 +43,27 @@ while :; do
 done
 
 lock_dir=/var/tmp/run-until-success
+if ! [[ -d "${lock_dir}" ]]; then
+    mkdir -p "${lock_dir}"
+fi
 task_lock="${lock_dir}/${task_name}.job"
 _mutex "${lock_dir}/${task_name}_mutex.pid" || {
     echo "Another instance is already running. Exiting..."
     exit 1
 }
 task_identifier_str="# run-until-success: ${task_name}"
-if ! [[ -d "${lock_dir}" ]]; then
-    mkdir "${lock_dir}"
-else
-    if [[ -f "${task_lock}" ]]; then
-        job="$(cat "${task_lock}")"
-        job_cmd_file="$(mktemp)"
-        if at -c "$job" >"${job_cmd_file}"; then
-            # make sure the job has our task identifier
-            if grep -q "${task_identifier_str}" "${job_cmd_file}"; then
-                # task is already queued
-                if ! (( force )); then
-                    exit 0
-                fi
-            fi
-        fi
+if [[ -f "${task_lock}" ]]; then
+  job="$(cat "${task_lock}")"
+  job_cmd_file="$(mktemp)"
+  if at -c "$job" >"${job_cmd_file}"; then
+    # make sure the job has our task identifier
+    if grep -q "${task_identifier_str}" "${job_cmd_file}"; then
+      # task is already queued
+      if ! (( force )); then
+        exit 0
+      fi
     fi
+  fi
 fi
 
 # Remove any stale lock
@@ -82,6 +81,6 @@ if [[ $rc -ne 0 ]] && [[ $rc -ne $stop_rc ]]; then
 ${task_identifier_str}
 "$0" --force "${orig_cmd_opt[@]}"
 EOF
-    job=$(cat "${at_output_file}" | cut -d' ' -f2)
+    job=$(grep "^job " "${at_output_file}" | cut -d' ' -f2)
     echo $job >"${task_lock}"
 fi
